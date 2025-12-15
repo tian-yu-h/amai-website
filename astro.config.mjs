203d93e194c9d6c7502aef19869a72498f3332c5
Tian Yu <tian.yuhank@gmail.com>
2025-12-15 12:30:43 -0700

    adjust the astro config mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://tian-yu-h.github.io',
-    base: '/amai-website/',
+  site: 'https://tian-yu-h.github.io',
+  base: '/amai-website',
   integrations: [mdx(), sitemap()],
-
   vite: {
     plugins: [tailwindcss()],
   },
